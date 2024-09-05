0b5367702dbfcbf7e04a6bc47a0f3b867e6fe670
MD. NAWROSE <124398938+MdNawrose@users.noreply.github.com>
2024-09-06 00:55:27 +0600

    1st Lab Assignment Q8

---
+++ Find the Sum of Elements in a List.py
@@ -0,0 +1,13 @@
+def sum_of_elements(lst):
+
+  
+    return sum(lst)
+
+
+lst = list(map(int, input("Enter The Elements: ").split()))
+
+
+result = sum_of_elements(lst)
+
+
+print(f"The Sum Of The Elements : {result}")
