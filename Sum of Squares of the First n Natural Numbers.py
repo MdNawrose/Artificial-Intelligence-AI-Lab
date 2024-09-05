9874c45617dd963f3d06bf0a02334798c9059cd9
MD. NAWROSE <124398938+MdNawrose@users.noreply.github.com>
2024-09-06 00:48:19 +0600

    1st Lab Assignment Q5

---
+++ Sum of Squares of the First n Natural Numbers.py
@@ -0,0 +1,13 @@
+def sum_of_squares(n):
+
+  
+    return (n * (n + 1) * (2 * n + 1)) // 6
+
+
+n = int(input("Enter the value of n: "))
+
+
+result = sum_of_squares(n)
+
+
+print(f"The sum of squares of the first {n} natural numbers is: {result}")
