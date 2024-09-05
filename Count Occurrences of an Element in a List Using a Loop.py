ae7624a798fa6e992c20175587407d723cb79c2c
MD. NAWROSE <124398938+MdNawrose@users.noreply.github.com>
2024-09-06 01:07:34 +0600

    1st Lab Assignment Q10

---
+++ Count Occurrences of an Element in a List Using a Loop.py
@@ -0,0 +1,16 @@
+def count_occurrences(lst, element):
+    count = 0
+
+    for item in lst:
+        if item == element:
+            count += 1
+    return count
+
+lst = input("Enter the elements of the list separated by spaces: ").split()
+
+element = input("Enter the element to count: ")
+
+result = count_occurrences(lst, element)
+
+print(f"The element '{element}' occurs {result} time(s) in the list.")
+
