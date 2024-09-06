7e9ed92fb5e8c10a7396ec61f0bb4c5a36e91e5c
MD. NAWROSE <124398938+MdNawrose@users.noreply.github.com>
2024-09-07 02:58:49 +0600

    1st Lab Assignment Q7

--- Check if an Element Exists in a List.py
+++ Check if an Element Exists in a List.py
@@ -1,15 +1,10 @@
 def check_element_exists(lst, element):
 
-  
     return element in lst
-
 
 lst = input("Enter The Elements : ").split()
 
-
 element = input("Enter The Element To Check: ")
-
-
 
 if check_element_exists(lst, element):
     print(f"The Element '{element}' Exists In The List.")
