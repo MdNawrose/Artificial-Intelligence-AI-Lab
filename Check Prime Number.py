e94127f9655e1e1768e17d0e2756099c6d614657
MD. NAWROSE <124398938+MdNawrose@users.noreply.github.com>
2024-09-07 02:57:55 +0600

    1st Lab Assignment Q2

--- Check Prime Number.py
+++ Check Prime Number.py
@@ -1,28 +1,16 @@
 def prime(num):
 
-
     if num < 2:
         return False
-
 
     for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
             return False
     return True
-
-
+    
 num = int(input("Enter A Number: "))
-
 
 if prime(num):
     print(f"{num} Is A Prime Number.")
 else:
     print(f"{num} Is Not A Prime Number.")
-
-
-
-
-
-
-
-Lab Assignment 1+2 (Q1)
