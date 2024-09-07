a818cebece46425df7a66cc12ccf36f5397ba024
MD. NAWROSE <124398938+MdNawrose@users.noreply.github.com>
2024-09-08 02:47:17 +0600

    1st Lab Assignment Q3

--- Find the Area and Circumference of a Circle.py
+++ Find the Area and Circumference of a Circle.py
@@ -2,11 +2,9 @@ def calculate_circle(r):
 
     a = 3.1416 * r ** 2
     c = 2 * 3.1416 * r
-    
     return a,c
                
 r = float(input("Enter The Radius Of The Circle: "))
-
 a,c = calculate_circle(r)
 
 print(f"Area Of The Circle: {a:.2f}")
