f45348c5b9fbd6bebe9c87c0676834a0852feb16
MD. NAWROSE <124398938+MdNawrose@users.noreply.github.com>
2024-09-13 21:54:51 +0600

    1st Lab Assignment Q6

--- Interchange First and Last Elements in a List.py
+++ Interchange First and Last Elements in a List.py
@@ -1,17 +1,10 @@
 def interchange_first_last(lst):
-
   
     if len(lst) > 1:
-      
-        
         lst[0], lst[-1] = lst[-1], lst[0]
     return lst
 
-
 lst = input("Enter The Elements : ").split()
-
-
 result = interchange_first_last(lst)
-
 
 print("List After Interchange :", result)
