a79ee05efb42cad8c7c92906b73f1e6f2f7044ad
Md. Nawrose <124398938+MdNawrose@users.noreply.github.com>
2024-09-21 23:52:15 +0600

    Python Lab Task

---
+++ Process Lists.py
@@ -0,0 +1,14 @@
+def process_lists(list1, list2):
+
+    concatenated_list = list1 + list2
+    sorted_list = sorted(concatenated_list)
+    reversed_sorted_list = sorted_list[::-1]
+
+    return reversed_sorted_list
+
+
+list1 = [1,2,3,4]
+list2 = [5,6,7,8]
+
+result = process_lists(list1, list2)
+print("Reversed sorted list:", result)
