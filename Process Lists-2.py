e19b6c966e5c27d56ec0ecab0518048ee9f92010
Md. Nawrose <124398938+MdNawrose@users.noreply.github.com>
2024-09-22 00:39:05 +0600

    Beecrowd

---
+++ Process Lists-2.py
@@ -0,0 +1,14 @@
+def process_lists(l1, l2):
+
+    c = l1 + l2
+    s = sorted(c)
+    r = s[::-1]
+
+    return r
+
+
+l1 = [3, 1, 4, 1]
+l2 = [9, 2, 6, 5]
+
+
+print("Reversed sorted list:", result)
